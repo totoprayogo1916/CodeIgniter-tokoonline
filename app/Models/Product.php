93a31141dcbcf1bb88eadb578ba70d1e1226357f
Toto <totoprayogo1916@gmail.com>
2023-05-22 16:36:23 +0700

    Update and rename model_products.php to Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Model_products extends CI_Model {
+class Product extends CI_Model {
 
 	public function all(){
 		//query semua record di table products
