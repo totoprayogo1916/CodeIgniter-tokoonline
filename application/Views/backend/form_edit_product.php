27eef483cba6c4e7f176b7079b5dbf3751ff862c
totoprayogo1916 <totoprayogo1916@gmail.com>
2023-05-23 20:59:08 +0700

    fix layout

--- application/Views/backend/form_edit_product.php
+++ application/Views/backend/form_edit_product.php
@@ -6,12 +6,8 @@
 	<!-- Load JQuery dari CDN -->
 	<script type="text/javascript" language="javascript" src="//code.jquery.com/jquery-1.10.2.min.js"></script>
 
-	<!-- Load DataTables dan Bootstrap dari CDN -->
-	<script type="text/javascript" language="javascript" src="//cdn.datatables.net/1.10.4/js/jquery.dataTables.min.js"></script>
-	<script type="text/javascript" language="javascript" src="//cdn.datatables.net/plug-ins/9dcbecd42ad/integration/bootstrap/3/dataTables.bootstrap.js"></script>
-
+	<!-- Load Bootstrap dari CDN -->
 	<link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="//cdn.datatables.net/plug-ins/9dcbecd42ad/integration/bootstrap/3/dataTables.bootstrap.css">
 </head>
 
 <body>
@@ -22,7 +18,7 @@
 			<div class="col-md-10">
 				<h1>Edit Product</h1>
 				
-				<?= form_open_multipart(route_to('admin.product.update', $product->id), ['class' => 'form-horizontal']) ?>
+				<?= form_open_multipart(route_to('admin.product.update', $product->id), ['class' => 'form-horizontal'], ['id' => $product->id]) ?>
 
 				<div class="form-group">
 					<label for="inputEmail3" class="col-sm-2 control-label">Product Name</label>
@@ -71,11 +67,6 @@
 		</div>
 	</div>
 
-	<script>
-		$(document).ready(function() {
-			$('#myTable').DataTable();
-		});
-	</script>
 </body>
 
 </html>
