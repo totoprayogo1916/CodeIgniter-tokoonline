193e9cd0232aa13ae568d920157ac2d430a7c81d
Toto <totoprayogo1916@gmail.com>
2023-05-29 16:04:22 +0700

    hapus kursus-php.com (#10)

--- application/Views/show_cart.php
+++ application/Views/show_cart.php
@@ -9,7 +9,7 @@ $cart = new Cart();
 <html lang="en">
 
 <head>
-	<title>Front-End Toko Online by Kursus-PHP.com</title>
+	<title>Front-End Toko Online</title>
 	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" />
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
